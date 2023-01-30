c968b8f07269b38240baf8d36fee3807e31b4ad6
Ahmed Bika <74166118+a7medbika@users.noreply.github.com>
2023-01-30 10:14:34 +0200

    Add project files.

---
+++ Videos_Folder_Length/CustomTextBox.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Videos_Folder_Length
+{
+    public class CustomTextBox : System.Windows.Controls.TextBox
+    {
+        // Place Holder Text Paramter
+        private string placeholder = "";
+        public string PlaceHolder
+        {
+            set { placeholder = value; Text = placeholder; }
+            get { return placeholder; }
+        }
+
+        // Border Radius Parameter
+        private int br = 0;
+        public int Br
+        {
+            set { br = value; ConfigureCornerRadius(br); }
+            get { return br; }
+        }
+
+        public void ConfigureCornerRadius(int r)
+        {
+            // Create New Style For type Border
+            Style style = new Style(typeof(Border));
+            // Configure Border style
+            Setter setter = new Setter()
+            {
+                Property = Border.CornerRadiusProperty,
+                Value = new CornerRadius(r)
+            };
+            style.Setters.Add(setter);
+            // Add styled border to textbox resources
+            Resources.Add(typeof(Border), style);
+        }
+
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            if (Text == PlaceHolder)
+                Text = "";
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(Text))
+                Text = PlaceHolder;
+        }
+    }
+
+    public class CustomButton : System.Windows.Controls.Button
+    {
+        // Border Radius Parameter
+        private int br = 0;
+        public int Br
+        {
+            set { br = value; ConfigureCornerRadius(br); }
+            get { return br; }
+        }
+
+        public void ConfigureCornerRadius(int r)
+        {
+            // Create New Style For type Border
+            Style style = new Style(typeof(Border));
+            // Configure Border style
+            Setter setter = new Setter()
+            {
+                Property = Border.CornerRadiusProperty,
+                Value = new CornerRadius(r)
+            };
+            style.Setters.Add(setter);
+            // Add styled border to textbox resources
+            Resources.Add(typeof(Border), style);
+
+            ConfigureHoverColor();
+        }
+
+        public void ConfigureHoverColor()
+        {
+            // Create New Style For type Button
+            Style style = new Style(typeof(Button));
+            // Trigger
+            Trigger trigger = new Trigger()
+            {
+                Property = IsMouseOverProperty,
+                Value = true
+            };
+            // Setter of trigger
+            Setter setter = new Setter()
+            {
+                Property = BackgroundProperty,
+                Value = new SolidColorBrush(Color.FromRgb(0, 0, 0))
+            };
+            // Add Setter to trigger
+            trigger.Setters.Add(setter);
+            // Add Trigger to Style
+            style.Triggers.Add(trigger);
+            // Add style
+            this.Resources.Add(typeof(Button), style);
+        }
+        
+    }
+
+
+
+}
---
+++ Videos_Folder_Length/MainWindow.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+
+namespace Videos_Folder_Length
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        IEnumerable<string> AllVideos;
+        int VideosCount = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog D = new FolderBrowserDialog();
+            if (D.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtPath.Text = D.SelectedPath;
+            }
+        }
+
+        private void BtnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtPath.Text == "Click Browse Button to select")
+            {
+                System.Windows.MessageBox.Show("Select The Folder First", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            AllVideos = Directory.GetFiles(txtPath.Text + "\\", "*.*", SearchOption.AllDirectories)
+                .Where(s => s.EndsWith(".mp4") || s.EndsWith(".mkv"));
+
+            if(AllVideos.Count() > 0)
+            {
+                VideosCount = AllVideos.Count();
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("There is no videos", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            lblResault.Visibility = Visibility.Visible;
+            lblCount.Text = VideosCount.ToString();
+
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            int Hours = 0;
+            int Mins = 0;
+            int Secs = 0;
+
+            foreach(var file in AllVideos)
+            {
+                using (ShellObject shell = ShellObject.FromParsingName(file))
+                {
+                    // alternatively: shell.Properties.GetProperty("System.Media.Duration");
+                    IShellProperty prop = shell.Properties.System.Media.Duration;
+                    // Duration will be formatted as 00:44:08
+                    string duration = prop.FormatForDisplay(PropertyDescriptionFormatOptions.None);
+
+                    // 0 : Hours, 1 : Mins, 2 : Secs
+                    string[] Times = duration.Split(':');
+                    Hours += Convert.ToInt32(Times[0]);
+                    Mins += Convert.ToInt32(Times[1]);
+                    Secs += Convert.ToInt32(Times[2]);
+                }
+            }
+
+            // Calc Total Lenght
+            lblTotalLength.Text = Calc_Total(Hours, Mins, Secs);
+            lblAvg.Text = Calc_Avg(Hours, Mins, Secs);
+
+
+            grdResault.Visibility = Visibility.Visible;
+        }
+
+        private string Calc_Total(int h, int m, int s)
+        {
+            m += s / 60;
+            s %= 60;
+
+            h += m / 60;
+            m %= 60;
+
+            // Control Formate to be like that:  00:01:01
+            string H = h.ToString();
+            string M = m.ToString();
+            string S = s.ToString();
+            while (H.Length < 2 || M.Length < 2 || S.Length < 2)
+            {
+                if (H.Length < 2)
+                    H = "0" + H;
+                if (M.Length < 2)
+                    M = "0" + M;
+                if (S.Length < 2)
+                    S = "0" + S;
+            }
+
+            return H + ":" + M + ":" + S;
+        }
+
+        private string Calc_Avg(int h, int m, int s)
+        {
+            // Get Total Time in Secounds
+            s += h * 3600;
+            s += m * 60;
+
+            // Avg in Secounds
+            int AvgS = s / VideosCount;
+
+            string H = (AvgS / 3600).ToString();
+            AvgS %= 3600;
+            string M = (AvgS / 60).ToString();
+            AvgS %= 60;
+            string S = AvgS.ToString();
+
+            // Control Formate to be like that:  00:01:01
+            while (H.Length < 2 || M.Length < 2 || S.Length < 2)
+            {
+                if (H.Length < 2)
+                    H = "0" + H;
+                if (M.Length < 2)
+                    M = "0" + M;
+                if (S.Length < 2)
+                    S = "0" + S;
+            }
+
+            return H + ":" + M + ":" + S;
+        }
+    }
+}
